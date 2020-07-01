c2ec0f230e79e4fbd830663633ba7907979e52d0
Finn <finnbutler1@gmail.com>
2020-07-01 17:50:53 +0100

    stock name abbrev and value layout set

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import React, {Component, useState} from 'react';
 import { Image, StyleSheet, Text, View, TextInput, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { AntDesign } from '@expo/vector-icons';
 import { Header, Button } from 'react-native-elements';
 import * as SQLite from 'expo-sqlite';
 
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 SQLite.openDatabase("database")
+
 
 function GetStocks() {
     // REPLACE THIS CODE WITH GETTING FROM DB
@@ -19,8 +22,18 @@ function GetStocks() {
         var tempStock = (
         <TouchableOpacity
             style={styles.button}
+            onPress={() => navigation.push()}
         >
-            <Text style={styles.buttonText}>Stock {i}</Text>
+            <View style={styles.stockContainer}>
+                <View style={styles.stockNameContainer}>
+                    <Text style={styles.stockAbbrev}>STCK</Text>
+                    <Text style={styles.stockName}>Stock {i}</Text>
+                </View>
+                <View style={styles.stockNameContainer}>
+                    <Text style={styles.stockValue}>120</Text>
+                    <Text style={styles.stockChange}>▲ 10</Text>
+                </View>
+            </View>
         </TouchableOpacity>);
         stocks[i] = (tempStock);
     }
@@ -114,16 +127,44 @@ const styles = StyleSheet.create({
         justifyContent: 'center',},
 
     button:{
-            alignSelf: "stretch",
-            height: 100,
-            justifyContent: "center",
-            backgroundColor: '#018c7a',
-            borderWidth: 1,
-            borderColor: "white",
+        alignSelf: "stretch",
+        height: 100,
+        justifyContent: "center",
+        backgroundColor: '#018c7a',
+        borderWidth: 1,
+        borderColor: "white",
         },
-    buttonText:{
+    stockContainer:{
+        flex: 1,
+        flexDirection: 'row',
+    },
+    stockNameContainer:{
+        flex: 3,
+        margin: 10,
+        flexDirection: 'column',
+    },
+    stockValueContainer:{
+        flex: 1,
+        margin: 10,
+        textAlignVertical: 'center',
+        flexDirection: 'column',
+    },
+    stockAbbrev: {
+        color: 'white',
+        fontSize: 48,
+    },
+    stockName:{
         color: "white",
-        alignSelf: 'center',
-        fontSize: 25,
+        fontSize: 16,
+    },
+    stockValue:{
+        textAlign: 'right',
+        color: "green",
+        fontSize: 48,
+    },
+    stockChange:{
+        textAlign: 'right',
+        color: "green",
+        fontSize: 16,
     }
 });
