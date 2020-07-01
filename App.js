8e38be3ea81c1ae5c89a49ae1c4bf15537771873
Finn <finnbutler1@gmail.com>
2020-07-01 19:40:49 +0100

    added navigation from pressing on stock. params not working doe

--- App.js
+++ App.js
@@ -7,12 +7,14 @@ import { AntDesign } from '@expo/vector-icons';
 import { Header, Button } from 'react-native-elements';
 import * as SQLite from 'expo-sqlite';
 import { SearchBar } from 'react-native-elements';
+import {createStackNavigator} from "@react-navigation/stack";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 //SQLite.openDatabase("database") i commented this out bc it wouldnt compile for me
 
 
-function GetStocks() {
+function GetStocks({ navigation }) {
     // REPLACE THIS CODE WITH GETTING FROM DB
 
     const stockNames = ["Banana ", "General Developments", "Citizen & Sons", "Vista plc", "Kent ", "Hall plc", "Hayre Utilities", "Butler Securities", "Southeast Oil", "Frontier Insurance", "Petroleum International", "Oil & Gas Holdings", "British Electric", "Anglo Pharmaceuticals", "Admiral Entertainment", "Compass ", "Expert Analytics", "Home Financial", "Imperial Cruiseline", "Intercontinental Airlines", "Global Gas", "BFS Foods", "Upward of Scotland", "Michaelangelo International", "Scott-Barnard plc", "Albert Technologies", "Standard Group", "Remco plc", "RDS Airlines", "Alliance International", "Cove  ", "BLL  ", "Evergreen Royal", "Alpine  ", "LDN Commerce", "New York Oil", "Enterprise Tobacco", "Churchill Hotels Group", "Cameron Industries", "Greyrock Servers", "Cactus ", "Caplin  ", "Lynx Group", "Charger ", "Lavalo & Barker", "Wroting Group", "Stout ", "Executive  Beverages", "Crandink Group", "Parkinson International"]
@@ -24,7 +26,10 @@ function GetStocks() {
         <TouchableOpacity
             key={i}
             style={styles.button}
-            onPress={() => navigation.push()}
+            onPress={() => navigation.push('Stock', {
+                stockName: stockNames[i],
+                stockAbbrev: stockAbbrevs[i],
+            })}
         >
             <View style={styles.stockContainer}>
                 <View style={styles.stockNameContainer}>
@@ -41,21 +46,40 @@ function GetStocks() {
         stocks[i] = (tempStock);
     }
     return (
+        <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView} alwaysBounceVertical={true} showsVerticalScrollIndicator={false}>
             <SearchBar
                 placeholder="Type Here..."
             />
             {stocks}
         </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+function Stock({ route, navigation }) {
+    const { stockName } = route.params;
+    const { stockAbbrev } = route.params;
+    return(
+        <View>
+            <Button
+                title= "Go back"
+                onPress={() =>
+                    navigation.pop()
+                }
+            />
+            <Text>{ stockName }</Text>
+            <Text>{ stockAbbrev }</Text>
+        </View>
     );
 }
 
 function Stocks() {
     return (
-        <SafeAreaView style={styles.container}>
-                <GetStocks />
-        </SafeAreaView>
-
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={GetStocks} />
+            <Stack.Screen name="Stock" component={Stock} />
+        </Stack.Navigator>
     );
 }
 
